40f7bf34532f18faac9299dfe002297da8bd4065
Ahmad Foroughi <pyhp2017@gmail.com>
2021-07-06 16:41:38 +0430

    upload base url added

--- API/App/app.js
+++ API/App/app.js
@@ -15,7 +15,7 @@ app.use(fileUpload({
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+app.use(`${process.env.BASE_URL}`+'/uploads', express.static(path.join(__dirname, 'uploads')))
 
 //================================== Routes ==================================\\
 app.use(`${process.env.BASE_URL}`+'/apply',require('./routers/apply'))
